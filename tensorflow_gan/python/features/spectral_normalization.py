Fix normalization to work with distribution strateiges.
PiperOrigin-RevId: 255665536
Change-Id: I9d42489ca54a1fe40078b1eba19f5c40e78ef37d

tensorflow_gan/python/features/spectral_normalization.py
@@ -88,11 +88,8 @@ def compute_spectral_norm(w_tensor, power_iteration_rounds=1,
                        "distribution strategy.")
     elif not tf.distribute.has_strategy():  # default strategy.
       aggregation = None
-    elif isinstance(replica_context, tf.distribute.MirroredStrategy()):
-      aggregation = tf.VariableAggregation.ONLY_FIRST_REPLICA
     else:
-      raise ValueError("spectral norm doesn't support replication strategy %s"
-                       % replica_context)
+      aggregation = tf.VariableAggregation.ONLY_FIRST_REPLICA
 
     u_var = tf.compat.v1.get_variable(
         _PERSISTED_U_VARIABLE_SUFFIX,