afb7eeb49d62566d12607c9369c21c7d1b26c194
Joel Shor <joelshor@google.com>
2019-10-23 03:11:04 -0700

    Update version number to reflect new release.
    PiperOrigin-RevId: 276241937
    Change-Id: Ie5e99a2290066fb650f85b94858c57c96816ede6

--- tensorflow_gan/python/version.py
+++ tensorflow_gan/python/version.py
@@ -16,7 +16,7 @@
 """Define TensorFlow GAN version information."""
 
 # We follow Semantic Versioning (https://semver.org/)
-_MAJOR_VERSION = '1'
+_MAJOR_VERSION = '2'
 _MINOR_VERSION = '0'
 _PATCH_VERSION = '0'
 
@@ -25,7 +25,7 @@ _PATCH_VERSION = '0'
 # stable release (indicated by `_VERSION_SUFFIX = ''`). Outside the context of a
 # release branch, the current version is by default assumed to be a
 # 'development' version, labeled 'dev'.
-_VERSION_SUFFIX = 'dev'
+_VERSION_SUFFIX = ''
 
 # Example, '0.4.0-dev'
 __version__ = '.'.join([
