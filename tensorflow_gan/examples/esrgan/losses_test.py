71f58ff83f8ee5f3e3769cc304310e2b7e0651b3
Nived P A <62166124+nivedwho@users.noreply.github.com>
2021-08-27 15:13:25 +0530

    Update losses_test.py
    Removed unnecessary imports

--- tensorflow_gan/examples/esrgan/losses_test.py
+++ tensorflow_gan/examples/esrgan/losses_test.py
@@ -15,10 +15,6 @@
 
 """Tests for tfgan.examples.esrgan.losses"""
 
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-
 from absl.testing import absltest
 import os
 
