efb0250282435cd4f5bf56ea8f0914b8f0b6f84a
Nived P A <62166124+nivedwho@users.noreply.github.com>
2021-08-27 14:50:43 +0530

    Update eval_test.py
    Variable name changes, removed unnecessary imports

--- tensorflow_gan/examples/esrgan/eval_test.py
+++ tensorflow_gan/examples/esrgan/eval_test.py
@@ -15,27 +15,22 @@
 
 """Tests for tfgan.examples.esrgan.eval"""
 
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-
-from absl.testing import absltest
-import os
 import collections
 
 import tensorflow as tf
-import eval_lib, networks
+import eval_lib
+import networks
 
-HParams = collections.namedtuple('HParams', [
+hparams = collections.namedtuple('hparams', [
   'num_steps', 'image_dir', 'batch_size', 'num_inception_images',
   'eval_real_images', 'hr_dimension', 'scale', 'trunk_size'])
 
 class EvalTest(tf.test.TestCase):
   def setUp(self):
-    self.HParams = HParams(1, '/content/', 
-                          2, 2, 
-                          True, 256, 
-                          4, 11)
+    self.hparams = hparams(1, '/content/', 
+                           2, 2, 
+                           True, 256, 
+                           4, 11)
     
     d = tf.data.Dataset.from_tensor_slices(tf.random.normal([2, 256, 256, 3]))
     def lr(hr):
@@ -45,10 +40,10 @@ class EvalTest(tf.test.TestCase):
     d = d.map(lr)
     d = d.batch(2)
     self.mock_dataset = d 
-    self.generator = networks.generator_network(self.HParams)
+    self.generator = networks.generator_network(self.hparams)
 
   def test_eval(self):
-    self.assertIsNone(eval_lib.evaluate(self.HParams, 
+    self.assertIsNone(eval_lib.evaluate(self.hparams, 
                                         self.generator, 
                                         self.mock_dataset))
 
