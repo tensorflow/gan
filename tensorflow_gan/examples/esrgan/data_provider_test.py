cbfbc2dbeeb8a03cd96ef2756185099a9be9b714
Nived P A <62166124+nivedwho@users.noreply.github.com>
2021-08-27 12:56:06 +0530

    Update data_provider_test.py
    Variable name changes, removed unnecessary imports,  moved `assertIsInstance()`

--- tensorflow_gan/examples/esrgan/data_provider_test.py
+++ tensorflow_gan/examples/esrgan/data_provider_test.py
@@ -14,34 +14,28 @@
 # limitations under the License.
 
 """Tests for tfgan.examples.esrgan.data_provider"""
-
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-
+import collections
 from absl.testing import absltest
-import numpy as np
 import tensorflow as tf
 import data_provider
-import collections
 
-Params = collections.namedtuple('HParams', ['hr_dimension', 
+
+hparams = collections.namedtuple('hparams', ['hr_dimension', 
                                             'scale', 
                                             'batch_size',
                                             'data_dir'])
 
 class DataProviderTest(tf.test.TestCase, absltest.TestCase):
-  
   def setUp(self):
     super(DataProviderTest, self).setUp()
-    self.HParams = Params(256, 4, 32, '/content/')
-    self.dataset = data_provider.get_div2k_data(self.HParams)
+    self.hparams = hparams(256, 4, 32, '/content/')
+    self.dataset = data_provider.get_div2k_data(self.hparams)
     self.mock_lr = tf.random.normal([32, 64, 64, 3])
     self.mock_hr = tf.random.normal([32, 256, 256, 3])
 
   def test_dataset(self):
+    self.assertIsInstance(self.dataset, tf.data.Dataset)
     with self.cached_session() as sess:
-      self.assertIsInstance(self.dataset, tf.data.Dataset)
       lr_image, hr_image = next(iter(self.dataset))
       sess.run(tf.compat.v1.global_variables_initializer())
 
